e657a85571bd5fe1535b5be35b3a627f10ecf8f4
walnads <s10158595@connect.np.edu.sg>
2019-08-20 12:59:21 +0800

    added Level-3 features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,29 +1,37 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.List;
+import java.io.*;
 
 public class Duke {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        List<String> strings = new ArrayList<>();
+        PrintStream out = new PrintStream(System.out, true, "UTF-8");
+        List<Task> tasks = new ArrayList<>();
 
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo + "\n" + "Hello! I'm Duke" + "\n" + "What can I do for you?");
+        out.println("Hello from\n" + logo + "\n" + "Hello! I'm Duke" + "\n" + "What can I do for you?");
 
         String input = sc.nextLine();
         while (!(input.equals("bye"))) {
             if (input.equals("list")) {
-                for (int i = 0; i < strings.size(); i++) {
-                    System.out.println((i+1) + ". " + strings.get(i));
+                out.println("Here are the tasks in your list: ");
+                for (int i = 0; i < tasks.size(); i++) {
+                    out.println((i+1) + ". " + tasks.get(i));
                 }
 
+            } else if (input.contains("done")) {
+                int id = Integer.parseInt(input.substring(input.length() - 1));
+                tasks.get(id - 1).markAsDone();
+                out.println("Nice! I've marked this task as done: " + "\n    " + tasks.get(id - 1));
+
             } else {
-                strings.add(input);
-                System.out.println("added: " + input);
+                tasks.add(new Task(input));
+                out.println("added: " + input);
             }
 
             input = sc.nextLine();
