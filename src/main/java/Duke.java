a78fb601ddc6a33161c0de9c55c60868da95bd47
walnads <s10158595@connect.np.edu.sg>
2019-08-20 20:31:27 +0800

    added Level-5 features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -18,33 +18,56 @@ public class Duke {
 
         String input = sc.nextLine();
         while (!(input.equals("bye"))) {
-            if (input.equals("list")) {
-                out.println("Here are the tasks in your list: ");
-                for (int i = 0; i < tasks.size(); i++) {
-                    out.println((i+1) + ". " + tasks.get(i));
+            try {
+                if (input.equals("list")) {
+                    out.println("Here are the tasks in your list: ");
+                    for (int i = 0; i < tasks.size(); i++) {
+                        out.println((i + 1) + ". " + tasks.get(i));
+                    }
+
+                } else if (input.contains("done")) {
+                    int id = Integer.parseInt(input.substring(input.length() - 1));
+                    tasks.get(id - 1).markAsDone();
+                    out.println("Nice! I've marked this task as done:\n    " + tasks.get(id - 1));
+
+                } else if (input.contains("todo")) {
+                    if (input.length() < 6) {
+                        throw new DukeException("\u2639 OOPS!!! The description of a todo cannot be empty.");
+
+                    } else {
+                        tasks.add(new Todo(input.substring(4)));
+                        out.println(String.format("Got it. I've added this task:\n  %s\nNow you have %s tasks in the list.",
+                                tasks.get(tasks.size() - 1), tasks.size()));
+                    }
+
+                } else if (input.contains("deadline")) {
+                    if (input.length() < 10) {
+                        throw new DukeException("\u2639 OOPS!!! The description of a deadline cannot be empty.");
+
+                    } else {
+                        String[] temp = input.substring(8).split(" /by ");
+                        tasks.add(new Deadline(temp[0], temp[1]));
+                        out.println(String.format("Got it. I've added this task:\n  %s\nNow you have %s tasks in the list.",
+                                tasks.get(tasks.size() - 1), tasks.size()));
+                    }
+
+                } else if (input.contains("event")) {
+                    if (input.length() < 7) {
+                        throw new DukeException("\u2639 OOPS!!! The description of a event cannot be empty.");
+
+                    } else {
+                        String[] temp = input.substring(5).split(" /at ");
+                        tasks.add(new Event(temp[0], temp[1]));
+                        out.println(String.format("Got it. I've added this task:\n  %s\nNow you have %s tasks in the list.",
+                                tasks.get(tasks.size() - 1), tasks.size()));
+                    }
+
+                } else {
+                    throw new DukeException("\u2639 OOPS!!! I'm sorry, but I don't know what that means :-(");
                 }
 
-            } else if (input.contains("done")) {
-                int id = Integer.parseInt(input.substring(input.length() - 1));
-                tasks.get(id - 1).markAsDone();
-                out.println("Nice! I've marked this task as done:\n    " + tasks.get(id - 1));
-
-            } else if (input.contains("todo")) {
-                tasks.add(new Todo(input.substring(4)));
-                out.println(String.format("Got it. I've added this task:\n  %s\nNow you have %s tasks in the list.",
-                        tasks.get(tasks.size()-1), tasks.size()));
-
-            } else if (input.contains("deadline")) {
-                String[] temp = input.substring(8).split(" /by ");
-                tasks.add(new Deadline(temp[0], temp[1]));
-                out.println(String.format("Got it. I've added this task:\n  %s\nNow you have %s tasks in the list.",
-                        tasks.get(tasks.size()-1), tasks.size()));
-
-            } else if (input.contains("event")) {
-                String[] temp = input.substring(5).split(" /at ");
-                tasks.add(new Event(temp[0], temp[1]));
-                out.println(String.format("Got it. I've added this task:\n  %s\nNow you have %s tasks in the list.",
-                        tasks.get(tasks.size()-1), tasks.size()));
+            } catch (DukeException e){
+                out.println(e.getMessage());
             }
 
             input = sc.nextLine();
